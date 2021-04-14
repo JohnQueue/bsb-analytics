5e4c8c6e2b9e03ec9cc534c294f1420a7dee81a4
JohnQueue <jcrquinanola@gmail.com>
2021-04-14 16:19:03 +0800

    UPDATE FILES

--- config.php
+++ config.php
@@ -1,19 +1,31 @@
 <?php
+// Show all errors
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // database hostname, you don't usually need to change this
-define('db_host', 'localhost');
+define('db_host','localhost');
 // database username
-define('db_user', 'phplogin');
+define('db_user','root');
 // database password
-define('db_pass', 'phplogin1234qwer');
+define('db_pass','');
 // database name
-define('db_name', 'phplogin');
+define('db_name','phplogin_mvc');
 // database charset, change this only if utf8 is not supported by your language
-define('db_charset', 'utf8');
+define('db_charset','utf8');
 // Email activation variables
 // account activation required?
-define('account_activation', false);
+define('account_activation',false);
 // Change "Your Company Name" and "yourdomain.com", do not remove the < and >
-define('mail_from', 'Your Company Name <noreply@yourdomain.com>');
-// Link to activation file, update this
-define('activation_link', 'http://yourdomain.com/phplogin/activate.php');
+define('mail_from','Your Company Name <noreply@yourdomain.com>');
+
+// Add-ons config
+define('csrf_protection',false);
+define('brute_force_protection',false);
+
+// MVC Config below
+// Enable/disable cache, only enable the cache in production mode
+define('cache_enabled',false);
+define('cache_path', BASE_PATH . '/cache/');
 ?>
