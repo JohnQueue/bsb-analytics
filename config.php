2e451967e1cfe848a7c402382a72a96d0a2402d3
JohnQueue <jcrquinanola@gmail.com>
2021-04-14 16:36:06 +0800

    Update config.php

--- config.php
+++ config.php
@@ -7,22 +7,22 @@ error_reporting(E_ALL);
 // database hostname, you don't usually need to change this
 define('db_host','localhost');
 // database username
-define('db_user','root');
+define('db_user','bsbanalytics');
 // database password
-define('db_pass','');
+define('db_pass','ZTihbWAebAMt');
 // database name
-define('db_name','phplogin_mvc');
+define('db_name','bsbanalytics');
 // database charset, change this only if utf8 is not supported by your language
 define('db_charset','utf8');
 // Email activation variables
 // account activation required?
-define('account_activation',false);
+define('account_activation',true);
 // Change "Your Company Name" and "yourdomain.com", do not remove the < and >
-define('mail_from','Your Company Name <noreply@yourdomain.com>');
+define('mail_from','Blood Sugar Blaster Analytics <admin@pconversions.com>');
 
 // Add-ons config
-define('csrf_protection',false);
-define('brute_force_protection',false);
+define('csrf_protection',true);
+define('brute_force_protection',true);
 
 // MVC Config below
 // Enable/disable cache, only enable the cache in production mode
